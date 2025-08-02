0348e402dcbd72a8890b6ed876455be67938bc66
Sergei Prilepko <sergey.prilepcko@gmail.com>
2025-08-02 03:06:43 +0300

    refactor: replace hardcoded step string with AUTH_STEPS constant in ValidateSubscriptionsStep and add useEffect for current step management in OnboardingPage

--- src/components/auth/steps/ValidateSubscriptions.tsx
+++ src/components/auth/steps/ValidateSubscriptions.tsx
@@ -14,6 +14,7 @@ import {
   useNavigateToOnboarding,
 } from "../../../hooks/useNavigateHooks";
 import { StepIndicator } from "../../ui/StepIndicator";
+import { AUTH_STEPS } from "../../../constants/auth";
 
 export const ValidateSubscriptionsStep: React.FC<{
   onBack: () => void;
@@ -44,7 +45,7 @@ export const ValidateSubscriptionsStep: React.FC<{
   };
 
   const handleAddNewChild = () => {
-    navigateToOnboarding({ step: "child" });
+    navigateToOnboarding({ step: AUTH_STEPS.CHILD });
   };
 
   const handleEditChildData = (childId: number) => {
