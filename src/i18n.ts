a8c094fc66b5746b1aa1c0b0e2c5231d493a11da
Sergei Prilepko <sergey.prilepcko@gmail.com>
2025-08-04 06:58:42 +0300

    refactor: standardize import statements and formatting in i18n configuration

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import ru from './locales/ru/translation.json';
-import uz from './locales/uz/translation.json';
-const LanguageDetector = require('i18next-browser-languagedetector').default;
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import ru from "./locales/ru/translation.json";
+import uz from "./locales/uz/translation.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(LanguageDetector)
@@ -12,14 +12,14 @@ i18n
       ru: { translation: ru },
       uz: { translation: uz },
     },
-    fallbackLng: 'ru',
+    fallbackLng: "ru",
     interpolation: {
       escapeValue: false,
     },
     detection: {
-      order: ['localStorage', 'navigator', 'htmlTag'],
-      caches: ['localStorage'],
+      order: ["localStorage", "navigator", "htmlTag"],
+      caches: ["localStorage"],
     },
   });
 
-export default i18n; 
+export default i18n;
